cb10022131f866110d54598f2acf6240a472f1fc
DRroth76 <david.randall.roth97@gmail.com>
2024-10-29 22:01:43 -0500

    Ran the test regression model

--- main.py
+++ main.py
@@ -128,12 +128,19 @@ def go(config: DictConfig):
             ##################
 
         if "test_regression_model" in active_steps:
+            mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest",
+                },
+            )
 
             ##################
             # Implement here #
             ##################
 
-            pass
 
 
 if __name__ == "__main__":
